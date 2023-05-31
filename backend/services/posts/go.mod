8d8bcbe8613578ce4185db5458c3bae08e333e44
Diar Begisbayev <diar_beg64@mail.ru>
2023-06-01 01:20:02 +0600

    posts service created

---
+++ backend/services/posts/go.mod
@@ -0,0 +1,29 @@
+module github.com/DiarCode/next-golang-chat-app/posts
+
+go 1.19
+
+require (
+	github.com/badoux/checkmail v1.2.1
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	go.uber.org/zap v1.24.0
+	golang.org/x/crypto v0.9.0
+	google.golang.org/grpc v1.55.0
+	google.golang.org/protobuf v1.30.0
+	gorm.io/driver/postgres v1.5.2
+	gorm.io/gorm v1.25.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+)
---
+++ backend/services/posts/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/DiarCode/next-golang-chat-app/posts/src/config"
+	"github.com/DiarCode/next-golang-chat-app/posts/src/database"
+	postspb "github.com/DiarCode/next-golang-chat-app/posts/src/gen/posts"
+	"github.com/DiarCode/next-golang-chat-app/posts/src/services"
+	"github.com/DiarCode/next-golang-chat-app/posts/src/utils"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	utils.InitLogger()
+
+	// config.Config = &config.AppConfig{
+	// 	APP_PORT:    50051,
+	// 	JWT_KEY:     os.Getenv("JWT_KEY"),
+	// 	DB_USER:     os.Getenv("DB_USER"),
+	// 	DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+	// 	DB_NAME:     os.Getenv("DB_NAME"),
+	// 	DB_PORT:     os.Getenv("DB_PORT"),
+	// }
+	config.AppConfig = &config.AppConfigType{
+		APP_PORT:    50052,
+		JWT_KEY:     "SSH256KEY",
+		DB_USER:     "postgres",
+		DB_PASSWORD: "postgres",
+		DB_NAME:     "meowchat_posts",
+		DB_PORT:     "5432",
+		DB_HOST:     "localhost",
+	}
+
+	database.ConnectDB()
+
+	server := grpc.NewServer()
+	postspb.RegisterPostsServiceServer(server, &services.PostsService{})
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", config.AppConfig.APP_PORT))
+	if err != nil {
+		utils.LoggerFatalf("Failed to listen: %v", err)
+	}
+
+	utils.LoggerInfof("Server is running on port %v", config.AppConfig.APP_PORT)
+
+	err = server.Serve(lis)
+	if err != nil {
+		utils.LoggerFatalf("Failed to serve: %v", err)
+	}
+}
