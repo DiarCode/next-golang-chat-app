0f39af22914fda41ef98022da8429026cb080efe
Diar Begisbayev <diar_beg64@mail.ru>
2023-06-06 02:18:17 +0600

    docker compose setup

--- backend/services/chat/main.go
+++ backend/services/chat/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 
 	"github.com/DiarCode/next-golang-chat-app/chat/src/config"
 	"github.com/DiarCode/next-golang-chat-app/chat/src/database"
@@ -15,23 +16,22 @@ import (
 func main() {
 	utils.InitLogger()
 
-	// config.Config = &config.AppConfig{
-	// 	APP_PORT:    50051,
-	// 	JWT_KEY:     os.Getenv("JWT_KEY"),
-	// 	DB_USER:     os.Getenv("DB_USER"),
-	// 	DB_PASSWORD: os.Getenv("DB_PASSWORD"),
-	// 	DB_NAME:     os.Getenv("DB_NAME"),
-	// 	DB_PORT:     os.Getenv("DB_PORT"),
-	// }
-	config.AppConfig = &config.AppConfigType{
-		APP_PORT:    50053,
-		JWT_KEY:     "SSH256KEY",
-		DB_USER:     "postgres",
-		DB_PASSWORD: "postgres",
-		DB_NAME:     "meowchat_chat",
-		DB_PORT:     "5432",
-		DB_HOST:     "localhost",
+	config.Config = &config.AppConfig{
+		APP_PORT:    os.Getenv("CHATS_APP_PORT"),
+		DB_USER:     os.Getenv("CHATS_DB_USER"),
+		DB_PASSWORD: os.Getenv("CHATS_DB_PASSWORD"),
+		DB_NAME:     os.Getenv("CHATS_DB_NAME"),
+		DB_PORT:     os.Getenv("CHATS_DB_PORT"),
 	}
+	// config.AppConfig = &config.AppConfigType{
+	// 	APP_PORT:    50053,
+	// 	JWT_KEY:     "SSH256KEY",
+	// 	DB_USER:     "postgres",
+	// 	DB_PASSWORD: "postgres",
+	// 	DB_NAME:     "meowchat_chat",
+	// 	DB_PORT:     "5432",
+	// 	DB_HOST:     "localhost",
+	// }
 
 	config.QueueConfig = &config.QueueConfigType{
 		KafkaURI: "localhost:9092",
--- backend/services/chat/src/config/app.config.go
+++ backend/services/chat/src/config/app.config.go
@@ -5,8 +5,6 @@ var AppConfig *AppConfigType
 type AppConfigType struct {
 	APP_PORT int
 
-	JWT_KEY string
-
 	DB_HOST     string
 	DB_USER     string
 	DB_PASSWORD string
